f5b93110939cc23309ba52aebef09bf030204102
christ0089 <christian-alva96@hotmail.com>
2019-03-16 09:09:34 -0600

    Coments

--- DevMty/View/TareaItemCS.cs
+++ DevMty/View/TareaItemCS.cs
@@ -9,13 +9,14 @@ namespace DevMty
         public TareaItemCS()
         {
             Title = "Tarea a Agregar";
-
+            // Initialize the all components
             var nameEntry = new Entry();
             nameEntry.SetBinding(Entry.TextProperty, "Name");
 
             var notesEntry = new Entry();
             notesEntry.SetBinding(Entry.TextProperty, "Notes");
 
+            // Initialize and set minimum date as the current date 
             var dateStart = new DatePicker
             {
                 MinimumDate = DateTime.Now,
@@ -23,22 +24,25 @@ namespace DevMty
 
             dateStart.SetBinding(DatePicker.DateProperty, "StartDate");
 
+            // Initialize and set minimum date as one day after the start date 
             var dateEnd = new DatePicker
             {
                 MinimumDate = dateStart.Date.AddDays(1)
             };
 
-
-            dateStart.DateSelected += (sender, e) => {
+            // Update endData minimum every time we change the start date so that startDate is never greater than
+            // end date
+            dateStart.DateSelected += (sender, e) =>
+            {
                 dateEnd.MinimumDate = e.NewDate.AddDays(1);
             };
-  
-
             dateEnd.SetBinding(DatePicker.DateProperty, "EndDate");
 
+            // Initialize the switch that will determine if a task is done or not.
             var doneSwitch = new Switch();
             doneSwitch.SetBinding(Switch.IsToggledProperty, "Done");
 
+            // Initialize Save Button as well as event handlers
             var saveButton = new Button { Text = "Guardar" };
             saveButton.Clicked += async (sender, e) =>
             {
@@ -48,21 +52,23 @@ namespace DevMty
                 await App.Database.SaveItemAsync(todoItem);
                 await Navigation.PopAsync();
             };
-
+            // Initialize Delelete Button as well as event handlers
             var deleteButton = new Button { Text = "Borrar" };
             deleteButton.Clicked += async (sender, e) =>
             {
                 var todoItem = (Models.Tarea)BindingContext;
-        
+
                 await App.Database.DeleteItemAsync(todoItem);
                 await Navigation.PopAsync();
             };
-
+            // Initialize Cancel Button as well as event handlers
             var cancelButton = new Button { Text = "Cancelar" };
             cancelButton.Clicked += async (sender, e) =>
             {
                 await Navigation.PopAsync();
             };
+
+            // Create Content View with all the components
             var content = new StackLayout
             {
                 Margin = new Thickness(20),
@@ -84,6 +90,8 @@ namespace DevMty
                     cancelButton
                 }
             };
+
+            // Set main view as a scroll view with the content inside.
             Content = new ScrollView
             {
                 Content = content
@@ -92,5 +100,5 @@ namespace DevMty
         }
 
     }
-}
 
+}
